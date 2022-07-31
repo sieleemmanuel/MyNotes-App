bfdd4008f5eeb19fb560f2d712ed1f8e8ee72f95
kimsiele <sielekim.emmanuel@gmail.com>
2022-07-31 22:06:53 +0300

    feat: note reminder functionality

--- app/src/main/java/com/developerkim/mytodo/ui/newnote/NewNoteFragment.kt
+++ app/src/main/java/com/developerkim/mytodo/ui/newnote/NewNoteFragment.kt
@@ -1,8 +1,9 @@
 package com.developerkim.mytodo.ui.newnote
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
-import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,132 +13,206 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.developerkim.mytodo.R
 import com.developerkim.mytodo.data.model.Note
 import com.developerkim.mytodo.data.model.NoteCategory
 import com.developerkim.mytodo.databinding.FragmentNewNoteBinding
 import com.developerkim.mytodo.ui.MainActivity
+import com.developerkim.mytodo.ui.listnotes.MainViewModel
 import com.developerkim.mytodo.util.HideKeyboard.hideKeyboard
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.developerkim.mytodo.util.NoteReminderWorker
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.time.LocalTime
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class NewNoteFragment : Fragment(), AdapterView.OnItemClickListener {
     private lateinit var selCategory: String
     private lateinit var binding: FragmentNewNoteBinding
-    private val viewModel: NewNoteViewModel by activityViewModels()
+    private val sharedViewModel: MainViewModel by activityViewModels()
+    private val TAG = NewNoteFragment::class.simpleName
+    private var categoryColor: Int? = null
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        setHasOptionsMenu(true)
-
         binding = FragmentNewNoteBinding.inflate(inflater)
-        val noteCategoriesAdapter = ArrayAdapter(
-            requireContext(),
-            R.layout.category_layout,
-            viewModel.noteCategories
-        )
-
-        val notePriorityAdapter = ArrayAdapter(
-            requireContext(),
-            R.layout.priority_layout,
-            viewModel.notePriorities
-        )
-
-        binding.apply {
-
-            selectCategory.apply {
-                setAdapter(noteCategoriesAdapter)
-                onItemClickListener = this@NewNoteFragment
-                setOnFocusChangeListener { view, hasFocus ->
+        sharedViewModel.categoriesNames.observe(viewLifecycleOwner) { categories->
+            val noteCategoriesAdapter = ArrayAdapter(
+                requireContext(),
+                R.layout.category_list_layout,
+                categories as MutableList<String>
+            )
+            binding.apply {
+                selectCategory.apply {
+                    setAdapter(noteCategoriesAdapter)
+                    onItemClickListener = this@NewNoteFragment
+                    setOnFocusChangeListener { view, hasFocus ->
+                        if (hasFocus) {
+                            hideKeyboard(view, requireActivity())
+                        }
+                    }
+                }
+                root.setOnFocusChangeListener { view, hasFocus ->
                     if (hasFocus) {
                         hideKeyboard(view, requireActivity())
                     }
                 }
-            }
-            selectPriority.apply {
-                setAdapter(notePriorityAdapter)
-                setOnFocusChangeListener { view, hasFocus ->
-                    if (hasFocus) {
-                        hideKeyboard(view, requireActivity())
-                    }
+                edtReminder.setOnClickListener {
+                    showReminderDatePicker()
                 }
+                /*colorSlider.setListener { _, color ->
+                sharedViewModel.getPickedColor(color)
+                categoryColor = color
+                Log.d(TAG, "string color:${color} ")
+            }*/
+                setUpToolbar()
             }
-            val noteTitle = noteTitleEditText.text
-            val noteText = noteTextEditText.text
-
-            clNewNote.setOnFocusChangeListener { view, hasFocus ->
-                if (hasFocus) {
-                    hideKeyboard(view, requireActivity())
-                }
-            }
-            menuItemClickListener(noteTitle, noteText)
         }
-       ( activity as MainActivity).onBackPressed()
-
         return binding.root
     }
-
-
-
-
-
 
     override fun onItemClick(parent: AdapterView<*>?, p1: View?, position: Int, id: Long) {
         selCategory = parent?.getItemAtPosition(position).toString()
     }
 
-    private fun FragmentNewNoteBinding.menuItemClickListener(
-        noteTitle: Editable?,
-        noteText: Editable
-    ) {
+    private fun FragmentNewNoteBinding.setUpToolbar() {
+        newNoteToolbar.setupWithNavController(findNavController())
         newNoteToolbar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.actionDone -> {
-                    selCategory = selectCategory.text.toString()
-                    val newNote = Note(
-                        selCategory,
-                        noteTitle.toString(),
-                        noteText.toString(),
-                        viewModel.noteDate
-                    )
-                    val noteList = viewModel.createNoteList(newNote)
-                    val categories = NoteCategory(
-                        newNote.noteCategory,
-                        noteList
-                    )
+                    sharedViewModel.pickedColor.observe(viewLifecycleOwner) { pickedColor: Int ->
+                        sharedViewModel.reminderTime.observe(viewLifecycleOwner) { reminder ->
+                            selCategory = selectCategory.text.toString()
+                            val newNote = Note(
+                                selCategory,
+                                noteTitleEditText.text.toString(),
+                                noteTextEditText.text.toString(),
+                                sharedViewModel.noteDate,
+                                reminderTime = reminder
+                            )
+                            val noteList = sharedViewModel.createNoteList(newNote)
 
-                    //Saving notes to database
-                    viewModel.updateIfExist(newNote, categories)
-                    findNavController().navigate(
-                        NewNoteFragmentDirections.actionNewNoteFragmentToListNotesFragment()
-                    )
+                            val categories = NoteCategory(
+                                newNote.noteCategory,
+                                noteList,
+                                pickedColor
+                            )
+                            sharedViewModel.insertCategoryAndNotes(newNote, categories)
+                            sharedViewModel.getCategories()
+                            setNoteReminder(newNote.reminderTime,newNote.noteTitle)
+                            findNavController().popBackStack()
+                        }
+                    }
+
+                    sharedViewModel.reminderTime()
                     true
+
                 }
                 R.id.actionClose -> {
-                    findNavController().navigateUp()
+                    findNavController().popBackStack()
                 }
-                R.id.actionAddReminder -> {
-                    showReminderDialog()
+                /*R.id.actionAddReminder -> {
+                    showReminderDatePicker()
                     true
-                }
+                }*/
                 else -> false
             }
         }
     }
 
-    private fun showReminderDialog() {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle(getString(R.string.add_reminder_title_label))
-            .setPositiveButton(
-                "SET"
-            ) { dialog, _ ->
-                dialog.dismiss()
-            }
-            .show()
+    private fun showReminderDatePicker() {
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .build()
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            val formattedDate = getSelectedDate(selection)
+            showTimePicker(formattedDate)
+        }
+        datePicker.isCancelable = false
+        datePicker.show(
+            (requireActivity() as MainActivity).supportFragmentManager,
+            "DATE_PICKER_TAG"
+        )
     }
 
+    @SuppressLint("SetTextI18n")
+    private fun showTimePicker(formattedDate: String?) {
+        val timePicker = MaterialTimePicker.Builder()
+            .setTitleText("Select Reminder Time")
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .setHour(LocalTime.now().hour)
+            .setMinute(LocalTime.now().minute)
+            .build()
+        timePicker.show(
+            (requireActivity() as MainActivity).supportFragmentManager,
+            "TIME_PICKER_TAG"
+        )
+        timePicker.addOnPositiveButtonClickListener {
+            val selectedHour = if (timePicker.hour < 10) {
+                "0${timePicker.hour}"
+            } else timePicker.hour
+            val minutes = if (timePicker.minute < 10) {
+                "0${timePicker.minute}"
+            } else timePicker.minute
+            val reminder = "$formattedDate ${selectedHour}:${minutes}:00"
+            binding.edtReminder.setText(reminder)
+            sharedViewModel.reminderTime(reminder)
+        }
+    }
+
+    private fun getSelectedDate(selection: Long): String? {
+        val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+        val timeZone = TimeZone.getDefault()
+        val offset = timeZone.getOffset(Date().time) * -1
+        val selectedDate = Date(selection + offset)
+        return formatter.format(selectedDate)
+    }
+
+    private fun createWorkerRequest(message: String, noteTitle:String,timeDelayInSec: Long) {
+        val reminderWorkRequest = OneTimeWorkRequestBuilder<NoteReminderWorker>()
+            .setInitialDelay(timeDelayInSec, TimeUnit.SECONDS)
+            .setInputData(
+                workDataOf(
+                    "title" to "Note Reminder",
+                    "message" to message,
+                    getString(R.string.note_title_arg_key) to noteTitle
+                )
+            )
+            .build()
+        WorkManager.getInstance(requireContext()).enqueue(reminderWorkRequest)
+    }
+
+    private fun getMilliFromDate(reminderDate: String, reminderTime: String): Long {
+        val timeFormatter = LocalTime.parse(reminderTime)
+        val timeMilliSec = timeFormatter.toSecondOfDay() * 1000L
+        val dateFormatter: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+        return dateFormatter?.parse(reminderDate).time.plus(timeMilliSec)
+    }
+
+    private fun setNoteReminder(reminderTime: String, noteTitle: String) {
+        val selectedDate = reminderTime.substringBefore(" ")
+        val selectedTime = reminderTime.substringAfter(" ")
+        Log.d(TAG, "Date and : $selectedDate Time:$selectedTime")
+        val selectedTimeInSec = getMilliFromDate(selectedDate, selectedTime)
+        val todayTimeInSec = Calendar.getInstance().timeInMillis
+        val delayInSeconds = (selectedTimeInSec.minus(todayTimeInSec)).div(1000L)
+
+        createWorkerRequest(
+            "Hi, it's time to check this $noteTitle note!!",
+            noteTitle,
+            delayInSeconds
+        )
+    }
 }
---
+++ app/src/main/java/com/developerkim/mytodo/util/NotificationHelper.kt
@@ -0,0 +1,53 @@
+package com.developerkim.mytodo.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.navigation.NavDeepLinkBuilder
+import com.developerkim.mytodo.R
+import com.developerkim.mytodo.util.Constants.Companion.CHANNEL_ID
+import com.developerkim.mytodo.util.Constants.Companion.NOTIFICATION_ID
+
+class NotificationHelper(val context: Context) {
+    fun createNotification(title:String, message:String, noteTitle:String){
+        createNotificationChannel()
+        val args = Bundle()
+        args.putString(context.getString(R.string.note_title_arg_key), noteTitle)
+        val pendingIntent = NavDeepLinkBuilder(context)
+            .setGraph(R.navigation.navigation)
+            .setDestination(R.id.readNotesFragment)
+            .setArguments(args)
+            .createPendingIntent()
+        val icon = BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_background)
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setLargeIcon(icon)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .build()
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notification)
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_ID,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "Note Reminder"
+            }
+            val notificationManager =context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
