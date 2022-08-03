5566b71b4e03c30b07743fdb7ec026cbd1bdba49
kimsiele <sielekim.emmanuel@gmail.com>
2022-08-03 22:16:14 +0300

    fix: reminder functionality refactoring

--- app/src/main/java/com/developerkim/mytodo/util/NoteReminderWorker.kt
+++ app/src/main/java/com/developerkim/mytodo/util/NoteReminderWorker.kt
@@ -3,13 +3,15 @@ package com.developerkim.mytodo.util
 import android.content.Context
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.developerkim.mytodo.R
 
-class NoteReminderWorker(workerParameters: WorkerParameters, val context: Context):Worker(context, workerParameters) {
+class NoteReminderWorker(val context: Context, workerParameters: WorkerParameters):Worker(context, workerParameters) {
     override fun doWork(): Result {
        NotificationHelper(context).createNotification(
-           inputData.getString("title").toString(),
-           inputData.getString("message").toString(),
-           inputData.getString("noteTitle").toString()
+           inputData.getString(context.getString(R.string.title_key))!!,
+           inputData.getString(context.getString(R.string.message_key))!!,
+           inputData.getString(context.getString(R.string.note_title_arg_key))!!,
+           inputData.getString(context.getString(R.string.note_category_arg_key))!!
        )
         return Result.success()
     }
--- app/src/main/java/com/developerkim/mytodo/util/NotificationHelper.kt
+++ app/src/main/java/com/developerkim/mytodo/util/NotificationHelper.kt
@@ -10,20 +10,23 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.NavDeepLinkBuilder
 import com.developerkim.mytodo.R
+import com.developerkim.mytodo.data.model.NoteCategory
 import com.developerkim.mytodo.util.Constants.Companion.CHANNEL_ID
 import com.developerkim.mytodo.util.Constants.Companion.NOTIFICATION_ID
 
 class NotificationHelper(val context: Context) {
-    fun createNotification(title:String, message:String, noteTitle:String){
+    fun createNotification(title:String, message:String, noteTitle:String, noteCategory: String){
         createNotificationChannel()
         val args = Bundle()
         args.putString(context.getString(R.string.note_title_arg_key), noteTitle)
+        args.putString(context.getString(R.string.note_category_arg_key), noteCategory)
         val pendingIntent = NavDeepLinkBuilder(context)
             .setGraph(R.navigation.navigation)
             .setDestination(R.id.readNotesFragment)
             .setArguments(args)
             .createPendingIntent()
         val icon = BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_background)
+
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_background)
             .setContentTitle(title)
